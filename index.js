22b13a7f369820aeeaf6260319a5254051ae7ac8
Taha <taha.othman@outlook.com>
2023-03-26 01:22:12 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array , string) {
+    return array.filter(item => item.toLowerCase() == string.toLowerCase());
+}
+
+function fuzzyMatch(array , string) {
+    return array.filter(item => item.slice(0,string.length) === string);
+}
+
+function matchName(array , string) {
+    return array.filter(item => item.name === string);
+}
